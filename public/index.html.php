8660463f813732c291afd30ea060046671cd38bc
BelaBelphegor <tiboitel@student.42.fr>
2018-01-23 18:39:41 +0100

    Aadding column brand on landing page.

--- public/index.html.php
+++ public/index.html.php
@@ -1,14 +1,12 @@
 <header class="masthead text-white text-center" style="background:url(https://c.pxhere.com/photos/dc/15/office_desk_business_working_mobile_phone_macbook_computer-860251.jpg!d) no-repeat center center fixed; background-size: cover;">
-	<div class="container">
-		<div class="row mt-5">
+	<div class="container my-auto py-auto align-middle">
+		<div class="row  mt-5">
 			<div class="col-xs-12 col-md-12 mx-auto mt-5">
 				<h1 class="text-uppercase mt-5 page-title">Monitor every click, tap and use.</h1>
-				<p class="font-weight-light mb-0 mt-2 header-description">Brand, track and optimize every touchpoint with Bitly, the world's leading link management platform. <a href="#">Learn more.</a>
+				<p class="font-weight-light mb-0 mt-2 header-description">Get in touch, track and analyze every touch with Tyrionurl, a dwarf in the court of the bigs ones. <a href="#">Learn more.</a>
 				</p>
 			</div>
 		</div>
-	</div>
-	<div class="container">
 		<div class="header-content-wrapper">
 			<div class="row mt-4">
 				<div class="col-xs-12 col-md-12 mx-auto">
@@ -26,7 +24,7 @@
 				</div>
 			</div>
 			<?php if (isset($shortened)) : ?>
-			<div class="bg-white rounded border border-dark p-4 mt-2 mb-4">
+			<div class="bg-white rounded border border-dark p-4 mt-2">
 				<div class="row">
 						<div class="col-xs-12 col-md-12 mx-auto text-left">
 							<a class="article-title" href="<?php echo (isset($origin_url)) ? h($origin_url) : ""; ?>">
@@ -34,7 +32,7 @@
 							</a>	
 						</div>
 					</div>
-				<div class="row mt-3 pb-3">
+				<div class="row mt-3">
 					<div class="col-xs-12 col-md-8 mx-auto text-left text-warning">
 						<a class="short-url" href="<?php echo (isset($shortened)) ? h($shortened) : ""; ?>"><?php echo (isset($shortened)) ? h($shortened) : ""; ?></a>
 						<button class="btn btn-outline-warning btn-sm" type="button">Copy</button>
@@ -48,3 +46,27 @@
 		</div>
 	</div>
 </header>
+<section class="main-content" id="main-content">
+	<div class="container p-2 pt-2 pb-5">
+		<div class="row">
+			<div class="col mx-auto main-subheader text-center">
+				<h2>Manage links on your web.</h2>
+				<p class="main-subheader">
+					Links are the threads that unite each of your Internet actions and those of your customers to a bigger story. Gather them to a better experience on each device with your content and skills.
+				</p>
+			</div>
+		</div>
+		<div class="row text-center">
+			<div class="col shorten-brand-case">
+				<h3>Brand</h3>
+				<p>When I'm going to make an another story, an another product</p>
+			</div>
+			<div class="col shorten-brand-case">
+				<h3>Track</h3>
+			</div>
+			<div class="col shorten-brand-case">
+				<h3>Custom Previews</h3>
+			</div>
+		</div>	
+	</div>
+</section>
